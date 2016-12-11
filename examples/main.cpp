fe7e0709c864ccd8a7fde7f2ee52a27877972a79
Loveskyrim <dr.konovalov@gmail.com>
2016-12-11 10:37:33 +0400

    Create main.cpp

---
+++ examples/main.cpp
@@ -0,0 +1,30 @@
+#include <stack.hpp>
+#include <iostream>
+
+int main()
+{
+    stack<int> st;
+    std::cout << st.count() << std::endl;
+
+    st.push(5);
+    st.push(6);
+    st.push(7);
+    std::cout << st.count() << std::endl;
+
+    try
+    {
+        std::cout << st.pop() << std::endl;
+        std::cout << st.pop() << std::endl;
+        std::cout << st.pop() << std::endl;
+        std::cout << st.pop() << std::endl;
+    }
+    catch (std::underflow_error & error)
+    {
+        std::cout << error.what() << std::endl;
+    }
+
+    std::cout << st.count() << std::endl;
+	std::cout << sizeof(short*);
+
+    return 0;
+}
